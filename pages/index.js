b21b7d644710c75a685790d5a34f30ef3e8b340d
izuuuu <isuru.hitinamaluwage@connect.qut.edu.au>
2023-05-28 15:10:22 +1000

    updated index.js

--- pages/index.js
+++ pages/index.js
@@ -150,8 +150,8 @@ export default function Home() {
           <div classname="basis-1/2 flex-1">
           <img src="next.png" className="rounded-lg object-cover" width={'100%'} height={'100%'} layout="responsive" ></img>
           </div>
-          <div classname="flex-1 width:100%">
-          <img src="mobile-app.gif" className="rounded-lg object-cover" width={'100%'} layout="responsive"></img>
+          <div classname="flex-1" style="width:100%">
+          <img src="mobile-app.gif" className="rounded-lg object-cover" style="width:100%" layout="responsive"></img>
           </div>
           <div classname="basis-1/2 flex-1">
           <img src="web2.png" className="rounded-lg object-cover" width={'100%'} height={'100%'} layout="responsive"></img>
